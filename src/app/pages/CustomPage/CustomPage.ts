6dadaf072b9be8d45289f294956c600d8bd17cbf
jsubarte <jsubarte@users.noreply.github.com>
2026-02-21 21:41:30 -0500

    CanFlyPipes completado

--- src/app/pages/CustomPage/CustomPage.ts
+++ src/app/pages/CustomPage/CustomPage.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { heroes } from '../../data/heroes';
 import { TogglecasePipe } from '../../pipes/togglecase-pipe';
+import { CanflyPipe } from '../../pipes/canfly-pipe';
 
 @Component({
   selector: 'app-custom-page',
-  imports: [ TogglecasePipe ],
+  imports: [ TogglecasePipe, CanflyPipe ],
   templateUrl: './CustomPage.html',
 })
 export default class CustomPage {
